83eaad93264991672089c6ab97500facd02cc30a
lynzz <lynzz168@gmail.com>
2015-12-24 10:35:46 +0800

    修改 web pack target

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,52 @@
 var vue = require('vue-loader')
 var webpack = require('webpack')
+var webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
 
-module.exports = {
+var options = {
   entry: './app/main.js',
   output: {
     path: './app',
     publicPath: '/app/',
     filename: 'app.js'
   },
-  target: 'atom',
 
   module: {
     loaders: [
       {
         test: /\.vue$/,
-        loader: vue.withLoaders({
-          // apply babel transform to all javascript
-          // inside *.vue files.
-          js: 'babel?optional[]=runtime&loose=all'
-        })
+        loader: 'vue'
       }, {
         test: /\.js?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel?optional[]=runtime&stage=0'
+        loader: 'babel'
+      }, {
+        // edit this for additional asset file types
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url',
+        query: {
+          // inline files smaller then 10kb as base64 dataURL
+          limit: 10000,
+          // fallback to file-loader with this naming scheme
+          name: '[name].[ext]?[hash]'
+        }
       }
     ]
+  },
+  vue: {
+    loaders: {
+      js: 'babel'
+    }
+  },
+  // configure babel-loader (for both .js and .vue files).
+  // see https://babeljs.io/docs/usage/options/
+  babel: {
+    presets: ['es2015', 'stage-0'],
+    plugins: ['transform-runtime']
   }
 };
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.plugins = [
+  options.plugins = [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
@@ -43,5 +60,9 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.optimize.OccurenceOrderPlugin()
   ]
 } else {
-  module.exports.devtool = '#source-map'
+  options.devtool = '#source-map';
 }
+
+options.target = webpackTargetElectronRenderer(options);
+
+module.exports = options;
